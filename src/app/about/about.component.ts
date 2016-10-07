c8b01266a9662af02bcc33b0e5e6c7802b652bb1
Kevin Chow <chowk3@uci.edu>
2016-10-07 10:28:28 -0700

    before major major change

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,14 +5,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
-
-  constructor() {
-    // Do stuff
-  }
-
-  ngOnInit() {
-    console.log('Hello About');
-  }
-
-}
+export class AboutComponent{}
